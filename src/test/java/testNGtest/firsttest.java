6276af33a6b37f1011026b3d4698f69c0d0aaceb
Aditya pathak <adityapathak@Adityas-MBP.lan>
2023-12-18 13:43:19 -0500

    First push from MacOs

---
+++ src/test/java/testNGtest/firsttest.java
@@ -0,0 +1,11 @@
+package testNGtest;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class firsttest {
+@Test
+	public void helloConsole() {
+		System.out.println("Hi Maven see you on Mac");
+	}
+}
